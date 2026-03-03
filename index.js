d21c26ae2b4a9c35aa9465849a6bc1dc1d182393
Jose Jesus <cp.josejesus@gmail.com>
2026-03-03 08:18:21 -0400

    added partial solution

---
+++ index.js
@@ -0,0 +1,21 @@
+import { CommandExecutor, Command, Commands } from "./services/command.js";
+
+globalThis.DOM = {};
+
+const DOM = globalThis.DOM;
+
+document.addEventListener("DOMContentLoaded", () => {
+  DOM.todoList = document.getElementById("todo-list");
+  DOM.addBtn = document.getElementById("add-btn");
+  DOM.todoInput = document.getElementById("todo-input");
+
+  DOM.addBtn.addEventListener("click", () => {
+    const cmd = new Command(Commands.ADD);
+    CommandExecutor.execute(cmd);
+  });
+
+  DOM.todoList.addEventListener("click", (event) => {
+    if (event.target.classList.contains("delete-btn")) {
+    }
+  });
+});
