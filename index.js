417fbe6a20d1ca69e3cef676eaba3cd606631698
Jose Jesus <cp.josejesus@gmail.com>
2026-03-06 08:36:41 -0400

    added solution

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+import { TodoList, TodoItem } from "./services/todoList.js";
 import { CommandExecutor, Command, Commands } from "./services/command.js";
+import { LocalStorage } from "./services/storage.js";
 
 globalThis.DOM = {};
 
 const DOM = globalThis.DOM;
+
+function renderList() {
+  const todos = TodoList.getInstance();
+  DOM.todoList.innerHTML = "";
+  for (let todo of todos.items) {
+    const listItem = document.createElement("li");
+    listItem.className = "todo-item";
+    listItem.innerHTML = `${todo.text} 
+                <button class="delete-btn">Delete</button>`;
+    listItem.dataset.text = todo.text;
+    DOM.todoList.appendChild(listItem);
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   DOM.todoList = document.getElementById("todo-list");
@@ -16,6 +31,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
   DOM.todoList.addEventListener("click", (event) => {
     if (event.target.classList.contains("delete-btn")) {
+      const todo = event.target.parentNode.dataset.text;
+      const cmd = new Command(Commands.DELETE, [todo]);
+      CommandExecutor.execute(cmd);
     }
   });
+
+  LocalStorage.load();
+
+  renderList();
+  TodoList.getInstance().addObserver(renderList);
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.ctrlKey && event.key === "p") {
+    event.preventDefault();
+    const cmd = new Command(Commands.ADD);
+    CommandExecutor.execute(cmd);
+  }
+  if (event.ctrlKey && event.key === "z") {
+    event.preventDefault();
+    const cmd = new Command(Commands.UNDO);
+    CommandExecutor.execute(cmd);
+  }
 });
