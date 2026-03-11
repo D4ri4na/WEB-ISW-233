6c83e197c0d6aeb271eccf2ded81927036f8f5de
Jose Jesus <cp.josejesus@gmail.com>
2026-03-10 22:48:47 -0400

    added ex

--- index.js
+++ index.js
@@ -9,34 +9,3 @@ const db = initMockDB({
 });
 const list = document.getElementById("list");
 const observerElement = document.getElementById("bottom-observer");
-
-const mutationObserver = new MutationObserver((mutations) => {
-  for (const { target, type } of mutations) {
-    if (type === "characterData" && SUPPORTED_ELEMENTS.has(target?.textContent)) {
-      const element = getHeading(target);
-      target.replaceWith(element);
-      element.focus();
-    }
-  }
-});
-
-let page = 0;
-const observer = new IntersectionObserver(
-  async ([bottom]) => {
-    if (bottom.isIntersecting) {
-      observerElement.textContent = "Loading";
-      const data = await db.getPage(page++);
-      const fragment = new DocumentFragment();
-      data.forEach(({ title, body }) => {
-        const card = createCardElement(title, body);
-        fragment.appendChild(card);
-        const content = card.querySelector(".card__body__content");
-        mutationObserver.observe(content, { characterData: true, subtree: true });
-      });
-
-      list.appendChild(fragment);
-    }
-  },
-  { threshold: 0.1 },
-);
-observer.observe(observerElement);
