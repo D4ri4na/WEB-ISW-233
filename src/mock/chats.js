ab4a82187cd8103a525631072e73af126f32245e
Jose Jesus <cp.josejesus@gmail.com>
2026-03-19 15:08:01 -0400

    added chat code

---
+++ src/mock/chats.js
@@ -0,0 +1,24 @@
+export const chats = [
+  {
+    id: 1,
+    title: "Welcome chat",
+    avatarUrl: "welcome.jpg",
+    unreadСount: 2,
+    lastMessage: {
+      author: "bot",
+      text: "<b>Hola bienvenidos!</b>",
+      time: "09:20",
+    },
+  },
+  {
+    id: 2,
+    title: "Frontend Crew",
+    avatarUrl: "frontend.jpg",
+    unreadСount: 0,
+    lastMessage: {
+      author: "Victor",
+      text: "Review on this saturday?",
+      time: "18:05",
+    },
+  },
+];
